437d70468594b099c8bbe55f08dd29dd3b4f88c6
nateshmbhat <nateshmbhat1@gmail.com>
2018-01-13 17:12:48 +0530

    added searching of elements in the untested soil table

--- public/assets/js/untestedSoil.js
+++ public/assets/js/untestedSoil.js
@@ -35,3 +35,26 @@ ref.once('value', snap=>{
   })
   
 })
+
+
+//Funtion used to search the elements in the untested soil table 
+function search_untested_soil_table(){
+  // Declare variables 
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("search_untested_soil_table");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("editable-datatable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
