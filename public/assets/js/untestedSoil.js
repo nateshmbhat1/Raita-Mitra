0dd8bd6b354dfa6b5c76fcb11683f11e0b60d70d
nateshmbhat <nateshmbhat1@gmail.com>
2018-01-13 16:09:53 +0530

    added untestedSoil dynamic updation by fetching data from the database

---
+++ public/assets/js/untestedSoil.js
@@ -0,0 +1,37 @@
+
+// firebase =  require("firebase") ; 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB-HyJyq5vB3kPf3rKYOtdQwpLmZX8v_PA",
+    authDomain: "kisanmitra-3895b.firebaseapp.com",
+    databaseURL: "https://kisanmitra-3895b.firebaseio.com",
+    projectId: "kisanmitra-3895b",
+    storageBucket: "kisanmitra-3895b.appspot.com",
+    messagingSenderId: "523180486596"
+  };
+firebase.initializeApp(config);
+
+
+
+ref = firebase.database().ref('/Soil Sample') ;
+console.log("This is doing ! ") ;
+ref.once('value', snap=>{
+  obj = snap.val() ;
+  Object.keys(obj).map((sample_no , index) =>{
+    data = obj[sample_no] ;
+    console.log("value obtained !") ;
+    
+    $('#untested_soil_data_entries').append(`
+    <tr>
+      <td>${index+1}</td>
+      <td><a href="javascript: void(0);" target="_blank" data-toggle="modal" data-target="#addDetails">${"NAME"}</a></td>
+      <td>${data.phone}</td>
+      ${(data.status=="pending")?`<td><span class="label label-danger">pending</span></td></tr>
+    `:`<td><span class="label label-success">completed</span></td>`}
+      </tr>
+  `) ; 
+
+   
+  })
+  
+})
