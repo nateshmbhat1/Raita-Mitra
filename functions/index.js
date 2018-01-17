8fa99b0056404bea91bb088c97fad8f9d51f84dc
nateshmbhat <nateshmbhat1@gmail.com>
2018-01-17 19:41:24 +0530

    updated

--- functions/index.js
+++ functions/index.js
@@ -11,9 +11,9 @@ app.use(express.static('../public')) ;
 
 request_controller(app) ; 
 
-// app.use((req ,res , next)=>{
-//     res.status(404).redirect('404.html') ; 
-// })
+app.use((req ,res , next)=>{
+    res.status(404).redirect('404.html') ; 
+})
 
 app.listen(8000) ; 
 
